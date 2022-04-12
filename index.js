709afe8a3cbb9512fa2743b1360125315d2ccb22
nova <nova.corps1010@outlook.com>
2022-04-11 21:55:36 -0400

    weight converter complete

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('output').style.visibility = 'hidden';
+const inputConverter = document.getElementById('lbsInput');
+inputConverter.addEventListener('input', function(e) {
+    document.getElementById('output').style.visibility = 'visible';
+    let lbs = e.target.value;
+    document.getElementById('gramsOutput').innerHTML = lbs / 0.0022046;
+    document.getElementById('kgOutput').innerHTML = lbs / 2.2046;
+    document.getElementById('ozOutput').innerHTML = lbs * 16;
+});
